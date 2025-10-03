fff0edc2b4c9a40b915a4e09709832a92064d5ec
rukonuzzamantopu <rukonuzzamantopu@gmail.com>
2025-10-04 02:19:56 +0600

    Add implementations for Free Drinks and BMI Calculator tasks

--- JS-conditionals-tasks-main/1 Free Drinks.js
+++ JS-conditionals-tasks-main/1 Free Drinks.js
@@ -4,3 +4,11 @@ Free Drinks
     - Burger more than 500tk: free Coke
     - Else Coke: 30tk
 */
+var burger = 520;
+
+if(burger > 500){
+    console.log('Free Coke');
+}
+else{
+    console.log('Coke: 30tk');
+}
