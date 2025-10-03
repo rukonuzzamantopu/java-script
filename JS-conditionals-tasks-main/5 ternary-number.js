2b6177118461e0e20422d60466ad48dab78c44df
rukonuzzamantopu <rukonuzzamantopu@gmail.com>
2025-10-04 02:43:27 +0600

    Refactor ternary number and bus fare calculator implementations for clarity; remove commented-out code in tempCodeRunnerFile.js

--- JS-conditionals-tasks-main/5 ternary-number.js
+++ JS-conditionals-tasks-main/5 ternary-number.js
@@ -10,3 +10,16 @@ write a simple if-else.
 also, write it using ternary operator.
 
  */
+var num1 = 10; 
+var num2 = 5; 
+var result;
+if (num1 > num2) {
+    result = num1 * 2;
+} else {
+    result = num1 + num2;
+}
+console.log(result);
+
+// Ternary operator
+result = (num1 > num2) ? (num1 * 2) : (num1 + num2);
+console.log(result);
--- JS-conditionals-tasks-main/6 bus fare calculator.js
+++ JS-conditionals-tasks-main/6 bus fare calculator.js
@@ -6,3 +6,25 @@ Ticket fare Calculator
     - Senior citizens (age >= 60) gets a 15% Discount
     - Otherwise Regular ticket fare 800 tk
 */
+const age = 42;
+const student = true;
+const price = 800;
+
+if (age <= 10) {
+    console.log('You can eat for free');
+}
+else if (student === true) {
+    // 50% discount
+    const discount = price * 50 / 100;
+    const payAmount = price - discount;
+    console.log(payAmount)
+}
+else if(age >= 60){
+    // 15% discount
+    const discount = price * 15 / 100;
+    const payAmount = price - discount;
+    console.log(payAmount)
+}
+else {
+    console.log(price)
+}
