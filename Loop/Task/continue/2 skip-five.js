a9e74caf93ad1d27e7e92efeb3d15efdc2a3ad2a
rukonuzzamantopu <rukonuzzamantopu@gmail.com>
2025-10-05 00:29:17 +0600

    Add examples of using continue in loops to skip odd numbers and numbers divisible by 5

--- Loop/Task/continue/2 skip-five.js
+++ Loop/Task/continue/2 skip-five.js
@@ -1,3 +1,9 @@
 /*
 display odd number from 55 to 85 and skip the numbers divisible by 5.
 */
+for(let num = 61; num < 100; num=num+2){
+    if(num % 5 === 0) {
+        continue;
+    }
+    console.log(num);
+}
