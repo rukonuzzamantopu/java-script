d5554e35f15ee4102cc7446cebc09cc3d9aca16e
rukonuzzamantopu <rukonuzzamantopu@gmail.com>
2025-10-05 00:30:28 +0600

    Add break examples in loops for stopping conditions

--- Loop/Task/break/3 square break.js
+++ Loop/Task/break/3 square break.js
@@ -1,3 +1,9 @@
 /* 
 Write a loop that goes from 1 to 100, but stops (using break) when it encounters the first square number (like 4, 9, 16, etc.)
 */
+for(let num = 1; num <= 100; num++){
+    if(Number.isInteger(Math.sqrt(num))) {
+        break;
+    }
+    console.log(num);
+}
