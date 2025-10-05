0149de4786275ae5bac6128e98829227eacb32e0
rukonuzzamantopu <rukonuzzamantopu@gmail.com>
2025-10-05 22:59:54 +0600

    add task file

---
+++ Object/Task/task4.js
@@ -0,0 +1,18 @@
+/*
+Task-4
+Count the number of properties.
+
+Input:
+
+let student = {
+    name: 'Ariana Grande',
+    age: 21,
+    city: 'Gaibandha',
+    isStudent: true
+};
+Output:
+
+
+4
+
+*/
---
+++ Object/Task/task5.js
@@ -0,0 +1,23 @@
+/*
+Task-5 (Hard)
+Loop through an object and print the key-value pairs with their types
+
+Input:
+
+let myObject = {
+
+name: 'John Doe',
+age: 25,
+city: 'Example City',
+isStudent: true
+};
+
+Output:
+
+
+key: name | type:  string
+key: age | type:  number
+key: city | type:  string
+key: isStudent | type:  boolean
+
+*/
