0faf2083e0706b67289674bd5c5354d43c4339eb
rukonuzzamantopu <rukonuzzamantopu@gmail.com>
2025-10-05 23:06:49 +0600

    add task solve

--- Object/Task/task4.js
+++ Object/Task/task4.js
@@ -16,3 +16,10 @@ Output:
 4
 
 */
+let student = {
+    name: 'Ariana Grande',
+    age: 21,
+    city: 'Gaibandha',
+    isStudent: true
+};
+console.log(Object.keys(student).length); // Output: 4
--- Object/Task/task5.js
+++ Object/Task/task5.js
@@ -21,3 +21,12 @@ key: city | type:  string
 key: isStudent | type:  boolean
 
 */
+let myObject = {
+    name: 'John Doe',
+    age: 25,
+    city: 'Example City',
+    isStudent: true
+};
+for (let key in myObject) {
+    console.log('key: ' + key + ' | type:  ' + typeof myObject[key]);
+}
