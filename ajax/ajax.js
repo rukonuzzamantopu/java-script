3ed236ed313e881156bd12f0e35b40e869d606d1
rukonuzzamantopu <rukonuzzamantopu@gmail.com>
2025-12-02 10:59:18 +0600

    add something

--- ajax/ajax.js
+++ ajax/ajax.js
@@ -1,22 +1,26 @@
-var pageCounter = 1;
+var pageCounter = 0;
 var animalContainar = document.getElementById("animal-info");
 var btn = document.getElementById("btn");
 
 // Function to handle the button click
 btn.addEventListener("click", function(){
+    pageCounter++;
+     if (pageCounter > 1) {
+        btn.classList.add("hide-me");
+    }
+    else {
     var ourRequest = new XMLHttpRequest();
-    ourRequest.open('GET', 'https://learnwebcode.github.io/json-example/animals-' + pageCounter + '.json');
+    ourRequest.open('GET', 'https://learnwebcode.github.io/json-example/animals-1.json');
     ourRequest.onload = function(){
         var ourData = JSON.parse(ourRequest.responseText);
         renderAnimal(ourData);
+        
     };
     ourRequest.send();
-    pageCounter++;
+}
 
     // Hide button after 3 clicks
-    if (pageCounter > 3) {
-        btn.classList.add("hide-me");
-    }
+    
 });
 
 // Function to render the fetched data
@@ -25,25 +29,25 @@ function renderAnimal(data) {
 
     // Loop through the array of animals
     for (i = 0; i < data.length; i++) {
-        readData += "<p>" + data[i].name + " is a " + data[i].species + " that likes to eat ";
+        readData += "<p>" + data[i].name + " nothing " + data[i].species + " like this ";
 
         // Loop through the 'likes' foods array
         for (ii = 0; ii < data[i].foods.likes.length; ii++) {
             if (ii == 0) {
                 readData += data[i].foods.likes[ii];
             } else {
-                readData += " and " + data[i].foods.likes[ii];
+                readData += " or " + data[i].foods.likes[ii];
             }
         }
 
-        readData += " and dislikes ";
+        readData += " nothing and also dislike ";
 
         // Loop through the 'dislikes' foods array
         for (ii = 0; ii < data[i].foods.dislikes.length; ii++) {
             if (ii == 0) {
                 readData += data[i].foods.dislikes[ii];
             } else {
-                readData += " and " + data[i].foods.dislikes[ii];
+                readData += " or " + data[i].foods.dislikes[ii];
             }
         }
 
